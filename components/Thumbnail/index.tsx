2a0c4ee9d980f60c575924eab01f0237d5e7c4a4
haseeb-ali-dev <h45.busy@gmail.com>
2023-01-05 06:35:15 -0800

    row modal trailers

--- components/Thumbnail/index.tsx
+++ components/Thumbnail/index.tsx
@@ -1,13 +1,27 @@
 import Image from "next/image"
-import Movie from "typings/movie"
+import { useSetRecoilState } from "recoil"
+
+import { modalState, movieState } from "atoms/modalAtom"
+import { Movie } from "typings"
 
 interface Props {
   movie: Movie
 }
 
 const Thumbnail = ({ movie }: Props) => {
+  const setCurrentMovie = useSetRecoilState(movieState)
+  const setShowModal = useSetRecoilState(modalState)
+
+  const handleClick = () => {
+    setCurrentMovie(movie)
+    setShowModal(true)
+  }
+
   return (
-    <div className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105">
+    <div
+      className="relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={handleClick}
+    >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
